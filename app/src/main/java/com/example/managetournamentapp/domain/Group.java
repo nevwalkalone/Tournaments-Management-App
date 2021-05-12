03212f766698a0d3e54b356ee169d51b654d318d
ChristoforosTrakas <christrakas7@gmail.com>
2021-05-12 18:02:35 +0300

    some methods

--- app/src/main/java/com/example/managetournamentapp/domain/Group.java
+++ app/src/main/java/com/example/managetournamentapp/domain/Group.java
@@ -7,8 +7,21 @@ import java.util.Map;
 public class Group {
     private boolean isKnockout;
     private int groupSize;
+    private int gamesNumber;
     private ArrayList<Game> games = new ArrayList<>();
     private Map <Team, Integer> ranking = new HashMap();
+
+    public Group(boolean isKnockout) {
+        this.isKnockout =isKnockout;
+        if (isKnockout){
+            groupSize = 2;
+            gamesNumber = 1;
+        }else{
+            groupSize = 4;
+            gamesNumber = 6;
+        }
+    }
+
 
 
     public void addTeam(Team team){
@@ -18,19 +31,41 @@ public class Group {
         if (ranking.keySet().size()==groupSize){
             return;
         }
+        if ( ranking.containsKey(team) )
+            return;
+        ranking.put(team, 0);
     }
 
-
-
-    public Group(boolean isKnockout) {
-        this.isKnockout =isKnockout;
-        if (isKnockout){
-            groupSize = 2;
+    public void removeTeam(Team team){
+        if (team == null){
+            return;
         }
-        else{
-            groupSize=4;
+        if ( !ranking.containsKey(team) ) {
+            return;
         }
+        ranking.remove(team);
     }
+
+    public int getTeamRanking(Team team){
+        if (team == null){
+            return -1;
+        }
+        if ( !ranking.containsKey(team) ) {
+            return -1;
+        }
+        return ranking.get(team);
+    }
+
+    public void setTeamRanking(Team team , int rank){
+        if (team == null || rank<0 ){
+            return ;
+        }
+        if ( !ranking.containsKey(team) ) {
+            return ;
+        }
+        ranking.put(team, rank);
+    }
+
 
     public boolean isKnockout() {
         return isKnockout;
@@ -44,8 +79,25 @@ public class Group {
         return games;
     }
 
-    public void setGames(ArrayList<Game> games) {
-        this.games = games;
+    public void addGame(Game game) {
+        if (game == null){
+            return;
+        }
+        if (games.size()==groupSize){
+            return;
+        }
+        if ( games.contains(game) )
+            return;
+        games.add(game);
+    }
+
+    public void removeGame(Game game) {
+        if (game == null){
+            return;
+        }
+        if ( !games.contains(game) )
+            return;
+        games.remove(game);
     }
 
     public Map<Team, Integer> getRanking() {
