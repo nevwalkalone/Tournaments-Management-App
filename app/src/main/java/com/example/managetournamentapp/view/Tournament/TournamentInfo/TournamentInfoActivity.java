b6c26cc713b54640cf7ac3646ee8544d9623f04e
Nondas <nevwalkalone13@gmail.com>
2021-06-02 19:29:45 +0300

    Some fixes

--- app/src/main/java/com/example/managetournamentapp/view/Tournament/TournamentInfo/TournamentInfoActivity.java
+++ app/src/main/java/com/example/managetournamentapp/view/Tournament/TournamentInfo/TournamentInfoActivity.java
@@ -1,4 +1,22 @@
 package com.example.managetournamentapp.view.Tournament.TournamentInfo;
 
-public class TournamentInfoActivity {
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.managetournamentapp.R;
+
+public class TournamentInfoActivity extends AppCompatActivity implements TournamentInfoView {
+    private TournamentInfoViewModel viewModel;
+    Button btnEditTournament;
+    Button btnDeleteTournament;
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tournament_info);
+    }
+
 }
