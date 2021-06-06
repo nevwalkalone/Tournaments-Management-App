cf80c68c3033949e3c8b55b89c9cccdfdd6cae03
ChristoforosTrakas <christrakas7@gmail.com>
2021-06-06 07:28:48 +0300

    tests in place

--- app/src/androidTest/java/com/example/managetournamentapp/view/User/RegisterOrganizer/RegisterOrganizerActivityTest.java
+++ app/src/androidTest/java/com/example/managetournamentapp/view/User/RegisterOrganizer/RegisterOrganizerActivityTest.java
@@ -1,24 +1,31 @@
-package com.example.managetournamentapp.viewTest.UserTest.RegisterOrganizerTest;
+package com.example.managetournamentapp.view.User.RegisterOrganizer;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Before;
 import org.junit.Rule;
 
+import com.example.managetournamentapp.R;
 import com.example.managetournamentapp.view.Organizer.OrganizerPage.OrganizerPageActivity;
 import com.example.managetournamentapp.view.User.RegisterOrganizer.RegisterOrganizerActivity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class RegisterOrganizerActivityTest {
 
     @Rule
-    public ActivityTestRule<RegisterOrganizerActivity> rule = new ActivityTestRule<>(RegisterOrganizerActivity.class);
+    public IntentsTestRule<RegisterOrganizerActivity> rule = new IntentsTestRule<>(RegisterOrganizerActivity.class);
 
     private RegisterOrganizerActivityObject activityObject;
 
@@ -27,8 +34,11 @@ public class RegisterOrganizerActivityTest {
         activityObject = new RegisterOrganizerActivityObject();
     }
 
+
     @Test
-    public void basicFillSetup(){
+    public void basicFill(){
+        onView(withId(R.id.saveOrganizerBtn)).check(matches( withText("SAVE")));
+
         activityObject
                 .fillField("username","user123")
                 .fillField("password","12345")
@@ -45,6 +55,7 @@ public class RegisterOrganizerActivityTest {
                 .pressSave();
 
         intended(hasComponent(OrganizerPageActivity.class.getName()));
+
 
     }
 
